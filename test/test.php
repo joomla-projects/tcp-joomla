378e337c6764d4136b8e5515f4653d7b010f75a4
David Jardin <d.jardin@djumla.de>
2018-03-30 17:23:42 +0200

    test correct behavior when no issues are found

---
+++ test/test.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @version    1.0.0
+ * @package    ${NAMESPACE}
+ * @copyright  Copyright (C) 2016 David Jardin - djumla Webentwicklung
+ * @license    GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link       http://www.djumla.de
+ */
+
+echo "Helloworld";
